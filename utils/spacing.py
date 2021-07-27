b061e419c749474b4d3b67a5968223685a99ce00
Joseph K J <josephkj20@gmail.com>
2021-07-27 13:34:01 +0000

    MDS

--- utils/spacing.py
+++ utils/spacing.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.manifold import MDS
+from scipy.spatial import distance
 from joblib import Parallel, delayed
 
 
@@ -18,6 +20,8 @@ class CentroidManager(object):
         self.n_clusters = n_clusters
         self.centroids = np.zeros((self.n_clusters, self.feature_dim))
         self.count = 100 * np.ones((self.n_clusters))
+        self.equi_dist_centroids = []
+        self.enable_mds_loss = False
         self.n_jobs = 1
 
     def parallel_dist_compute(self, X):
@@ -27,18 +31,29 @@ class CentroidManager(object):
         dist = np.hstack(dist)
         return dist
 
-    def init_clusters(self, X):
+    def init_clusters(self, X, enable_mds_loss=True):
         model = KMeans(n_clusters=self.n_clusters, n_init=20)
         model.fit(X)
         self.centroids = model.cluster_centers_
+        if enable_mds_loss:
+            self.enable_mds_loss = True
+            self.prepare_equidistant_centroids()
+
+    def prepare_equidistant_centroids(self):
+        self.max_dist = distance.pdist(self.centroids).max()
+        distances = self.max_dist * np.ones((self.n_clusters, self.n_clusters))
+        np.fill_diagonal(distances, 0)
+        self.equi_dist_centroids = MDS(n_components=self.feature_dim, dissimilarity='precomputed').fit(distances).embedding_
 
     def update_cluster(self, X, cluster_id):
         n_samples = X.shape[0]
         for i in range(n_samples):
             self.count[cluster_id] += 1
             eta = 1.0 / self.count[cluster_id]
-            updated_cluster = ((1 - eta) * self.centroids[cluster_id] +
-                               eta * X[i])
+            if self.enable_mds_loss:
+                updated_cluster = ((1 - eta) * (self.centroids[cluster_id] + self.equi_dist_centroids[cluster_id]) + eta * X[i])
+            else:
+                updated_cluster = ((1 - eta) * self.centroids[cluster_id] + eta * X[i])
             self.centroids[cluster_id] = updated_cluster
     
     def update_assingment(self, X):
